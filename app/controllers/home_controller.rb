a4f8769aca54bfd7506092a32079638fa4cd52cc
ujjawalchugh97 <ujjawalchugh97@gmail.com>
2017-07-28 13:16:43 +0530

    Create tweet and view your own tweets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
-  def index
-  end
+  before_action :authenticate_user!
+  
+    def index
+   		@tweets = Tweet.includes(:user).all.order(created_at: :desc).limit(50)
+    end
+  
+    def create_tweet
+   		current_user.tweets.create(content: params[:content])
+   		return redirect_to '/'
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
+  
+  root 'home#index'
 
+  devise_for :users
+  get '/' => 'home#index'
+
+  post '/create_tweet' => 'home#create_tweet'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
